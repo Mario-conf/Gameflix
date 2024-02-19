033f5ee8b6019d2713ac566a86afb32d1686f157
Mario AV <152533010+Mario-conf@users.noreply.github.com>
2024-02-19 11:54:54 +0100

    auctualizaciones

--- script.js
+++ script.js
@@ -8,32 +8,34 @@ async function loadMoviesFromJSON() {
   } catch (error) {
     console.error('Error loading movies:', error);
   }
-}function showMovies(movies) {
-    main.innerHTML = '';
-  
-    movies.forEach((movie) => {
-      const { title, image, rating, description, enlace } = movie;
-  
-      const movieEl = document.createElement('div');
-      movieEl.classList.add('movie');
-  
-      movieEl.innerHTML = `
-        <img src="${image}" alt="${title}">
-        <div class="movie-info">
-          <h3>${title}</h3>
-          <span class="${getClassByRate(rating)}">${rating}</span>
-        </div>
-        <div class="overview">
-          <h3>Overview</h3>
-          ${description}<br>
-          <a href="${enlace}" class="ver-ahora" target="_blank">Ver ahora</a>
-        </div>
-      `;
-  
-      main.appendChild(movieEl);
-    });
-  }
-  
+}
+
+function showMovies(movies) {
+  main.innerHTML = '';
+
+  movies.forEach((movie) => {
+    const { title, image, rating, description, enlace } = movie;
+
+    const movieEl = document.createElement('div');
+    movieEl.classList.add('movie');
+
+    movieEl.innerHTML = `
+      <img src="${image}" alt="${title}">
+      <div class="movie-info">
+        <h3>${title}</h3>
+        <span class="${getClassByRate(rating)}">${rating}</span>
+      </div>
+      <div class="overview">
+        <h3>Overview</h3>
+        ${description}<br>
+        <a href="${enlace}" class="ver-ahora" target="_blank">Ver ahora</a>
+      </div>
+    `;
+
+    main.appendChild(movieEl);
+  });
+}
+
 function getClassByRate(rating) {
   if (rating >= 8) {
     return 'green';
@@ -44,46 +46,59 @@ function getClassByRate(rating) {
   }
 }
 
-
 function searchMovies(event) {
-    event.preventDefault();
-    
-    const searchTerm = document.getElementById('search').value.trim().toLowerCase();
-  
-    if (searchTerm && searchTerm !== '') {
-      filterMovies(searchTerm);
+  event.preventDefault();
+  const searchTerm = document.getElementById('search').value.trim().toLowerCase();
+
+  if (searchTerm && searchTerm !== '') {
+    filterMovies(searchTerm);
+  } else {
+    loadMoviesFromJSON();
+  }
+  nav.forEach(nav_el => nav_el.classList.remove('visible'));
+}
+
+document.getElementById('about-link').addEventListener('click', function() {
+  nav.forEach(nav_el => nav_el.classList.remove('visible'));
+});
+
+document.getElementById('support-link').addEventListener('click', function() {
+  nav.forEach(nav_el => nav_el.classList.remove('visible'));
+});
+
+
+function filterMovies(searchTerm) {
+  fetch('datos.json')
+    .then((response) => response.json())
+    .then((data) => {
+      const filteredMovies = data.filter(movie => movie.title.trim().toLowerCase().includes(searchTerm));
+      showMovies(filteredMovies);
+      scrollAndHighlightFirstMovie();
+    })
+    .catch((error) => console.error('Error fetching movies:', error));
+}
+
+function scrollAndHighlightFirstMovie() {
+
+  const firstMovie = document.querySelector('.movie');
+  const mediaQuery = window.matchMedia('(max-width: 767px)');
+  if (firstMovie) {
+    firstMovie.scrollIntoView();
+    firstMovie.classList.add('highlight');
+    about.style.display = "none"; 
+
+    if (mediaQuery.matches) {
+      firstMovie.style.width = '100%';  
     } else {
-      loadMoviesFromJSON();
+      firstMovie.style.width = 'calc(3 * 180px - 3 * 10px)';
     }
   }
-  
-  function filterMovies(searchTerm) {
-    fetch('datos.json')
-      .then((response) => response.json())
-      .then((data) => {
-        const filteredMovies = data.filter(movie => movie.title.trim().toLowerCase().includes(searchTerm));
-        showMovies(filteredMovies);
-        scrollAndHighlightFirstMovie();
-      })
-      .catch((error) => console.error('Error fetching movies:', error));
-  }
-  
-  function scrollAndHighlightFirstMovie() {
-    // Obtenemos el primer elemento con la clase 'movie'
-    const firstMovie = document.querySelector('.movie');
-    
-    if (firstMovie) {
-      // Desplazamos hasta el primer elemento encontrado y lo enfocamos
-      firstMovie.scrollIntoView();
-      firstMovie.classList.add('highlight');
-      firstMovie.style.width = 'calc(3 * 180px - 3 * 10px)'; 
-    }
-  }
-  
-  window.addEventListener('load', loadMoviesFromJSON);
-  
-  const form = document.getElementById('form');
-  form.addEventListener('submit', searchMovies);
+}
+
+window.addEventListener('load', loadMoviesFromJSON);
+
+const form = document.getElementById('form');
+form.addEventListener('submit', searchMovies);
 
 
   function reloadPage() {
